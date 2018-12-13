0be5a13c7b058d6fd4d3eea7da6fe1f6ff6d8330
vivek sharma <vivek1@viveks-MacBook-Pro.local>
2018-12-13 19:06:21 +0530

    API call is working along with that update ui is also working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,40 +8,56 @@ import {from} from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'ang2php';
   pressmessage ="";
   nameFormControl = new FormControl('', Validators.required);
 
-  constructor() {
-    console.log("This is constructor");
-  }
 
   ngOnInit(){
     console.log("This is on ng init");
   }
+  constructor() {
+    console.log("This is constructor");
+  }
 
+  callmeAfter(){
+    console.log(" I a being called after subscribe");
+  }
   gotophp(){
     //only pass to java if value is present
     if(! this.nameFormControl.hasError('required')){
       console.log("Go to php");
       this.pressmessage="Going to fetch data from java";
-
+      let messtxt="";
       // Create an Observable out of a promise
       const data = from(fetch('http://localhost:8080/lottoget'));
       // Subscribe to begin listening for async result
-      data.subscribe({
-        next(response) {
+     /* data.subscribe({
 
-           console.log("returntxt " + response);
-          //  this.pressmessage= ("return val " + returntxt1);
-          },
+        next(response){
+          response.text().then((text)=>{
+            console.log("text-"+text);
+            this.pressmessage= "return val " + text;
+          });
+        },
+
         error(err) { console.error('Error: ' + err); },
         complete() { console.log('Completed'); }
       });
+      */
+      data.subscribe(resp=>{
+        let txtmsg=""
+        resp.text().then((text)=>{
+          console.log("Thsi is text-" +text) ;
+          txtmsg=text;
+            this.pressmessage= "return val " + txtmsg;
+        });
 
+      });
     }else{
       this.nameFormControl.markAsTouched();
 
